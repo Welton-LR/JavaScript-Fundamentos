dd8e150a38a0c07792330f30ad2fd14a6462818c
Welton <weltonrdesigner@gmail.com>
2024-02-04 17:39:24 -0300

    Create Functions

--- Aula 4/function.js
+++ Aula 4/function.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+// Ciando uma função
+
+function sayMayName(name)
+{
+   console.log('Your Name Is: ' + name);
+}
+
+// Chamando uma função
+sayMayName('Welton');
+
+// Agora outro exemplo. Aqui eu multipliquei dois valores e retonei o resultado.
+
+ function quadrado(valor)
+ {
+    return valor * valor;
+ }
+
+// Armazenei o valor (que minha função retornou) na variável quadradoDeDez
+ const quadradoDeDez = quadrado(10);
+ console.log(quadradoDeDez);
