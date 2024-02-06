b175c389ad662051d6f9425574487127cd73e998
Welton <weltonrdesigner@gmail.com>
2024-02-05 21:34:11 -0300

    Condicional OK

---
+++ if else/condicional.js
@@ -0,0 +1,15 @@
+// Aqui trabalharemos com as condicionais if e else.
+
+let driverSpeed = 120;
+
+if (driverSpeed > 110){
+    console.log('Driving too fast');
+}
+else if (driverSpeed > 40 && driverSpeed <= 110){
+    console.log('OK');
+}
+else
+    console.log('Driving too slow');
+
+
+
--- if else/if e else.js
+++
@@ -1,8 +0,0 @@
-// Aqui trabalharemos com as condicionais if e else.
-
-let driverSpeed = 80;
-
-if (driverSpeed > 110)
-{
-    console.log('Acima da velocidade permitida');
-}
