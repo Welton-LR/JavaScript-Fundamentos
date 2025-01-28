1e15465781e61da1e75ce2e478a77a58ed32643c
Welton <weltonrdesigner@gmail.com>
2025-01-27 21:26:25 -0300

    apredi usar um constructor

---
+++ constructor/constructor.js
@@ -0,0 +1,8 @@
+function Car(name, automaker, color){
+    this.nameCar = name;
+    this.automaker = automaker;
+    this.colorCar = color
+}
+
+const pulseT200 = new Car('Pulse T200', 'FIAT', 'black');
+console.log(pulseT200);
