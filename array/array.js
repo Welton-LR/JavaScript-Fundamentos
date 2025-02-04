eaffdd7878cfd0f88b0391734cf52b4e27f4a8bc
Welton <weltonrdesigner@gmail.com>
2025-02-03 21:20:35 -0300

    criando e adicionando valores em uma array

---
+++ array/array.js
@@ -0,0 +1,16 @@
+
+const animals = ["cat","dog","alligator"];
+
+//Adicionando elementos ao final do array (push)
+animals.push("capybara");
+
+//Adicionando elementos ao início da array (unshift)
+animals.unshift("badge","lion");
+
+/* Adicionando elementos a partir de um index (splice)
+O primeiro argumento é o index, o segundo é o número de elementos
+que serão apagados. O terceiro são os elementos adicionados*/
+animals.splice(3,0,"turtle","bear")
+
+// node ./array/array.js
+console.log(animals);
