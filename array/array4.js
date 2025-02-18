7276c861b40eee8036eee3db25da6a3ebd8cb243
Welton <weltonrdesigner@gmail.com>
2025-02-17 21:17:20 -0300

    Removendo elementos em uma array

---
+++ array/array4.js
@@ -0,0 +1,10 @@
+//Removendo elewmentos de uma array
+
+const tartarugasNinjas = ["Rafael","Leonardo","Miquelângelo","Donatelo"];
+console.log(tartarugasNinjas);
+tartarugasNinjas.pop(); // Removendo o último elemento
+console.log(tartarugasNinjas);
+tartarugasNinjas.shift(); //Removendo o primeiro elemento
+console.log(tartarugasNinjas);
+tartarugasNinjas.splice(0,2); //Escolhendo um índice e removendo dois elementos
+console.log(tartarugasNinjas);
